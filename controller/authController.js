330cb96397a839ecfe927a0ec4dc4d1e6b785d8a
gitDemo <sy9535393@gmail.com>
2025-07-24 22:28:16 +0530

    add user registration and validation

--- controller/authController.js
+++ controller/authController.js
@@ -1,3 +1,55 @@
-export const registerController = () => {
+import { hashPassword } from '../helpers/authHelper.js'
+import userModel from '../models/userModel.js'
+
+export const registerController = async (req, res) => {
+    try {
+        const { name, email, password, phone, address } = req.body
+
+        //validation
+        if (!name) {
+            return res.send({ error: "name is required" })
+        }
+        if (!email) {
+            return res.send({ error: "email is required" })
+        }
+        if (!password) {
+            return res.send({ error: "password is required" })
+        } if (!phone) {
+            return res.send({ error: "phone is required" })
+        }
+        if (!address) {
+            return res.send({ error: "address is required" })
+        }
+
+        //check user 
+        const exisitingUser = await userModel.findOne({ email })
+
+        //exisiting user
+        if (exisitingUser) {
+            return res.status(200).send({
+                success: false,
+                Message: "Already registration please Login"
+            })
+        }
+
+        //register user
+        const hashedPassword = await hashPassword(password)
+
+        //save
+        const user = await new userModel({ name, email, phone, address, password: hashedPassword }).save()
+        res.status(201).send({
+            success: true,
+            message: "user Registration successfully",
+            user
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "error in registration",
+            error
+        })
+
+    }
 
 }
